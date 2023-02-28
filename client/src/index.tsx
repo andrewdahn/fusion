1e76123277d28f5e325c89331b8e64b39b14edce
andrewdahn <andrewahnjr@gmail.com>
2023-02-27 21:46:39 -0800

    get rid of warning message

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './style.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(<App />);
