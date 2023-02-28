64e1dfa467a5a46019ff2e2127d11e0d32f2c9a5
andrewdahn <andrewahnjr@gmail.com>
2023-02-27 23:07:41 -0800

    set up fetch api

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import * as dotenv from 'dotenv';
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response } from 'express';
 import path from 'path';
+import axios from 'axios';
 dotenv.config();
 
 const app = express();
@@ -11,4 +12,23 @@ const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
+});
+
+app.get('/summaries', (req: Request, res: Response) => {
+  const { type, addresses } = req.query;
+  axios
+    .request({
+      method: 'POST',
+      url: `${process.env.URL}/bulk/${process.env.PROVIDER}/cluster/summary?type=${type}`,
+      data: { addresses },
+      headers: {
+        api_key: process.env.KEY,
+      },
+    })
+    .then(({ data }) => {
+      res.status(200).send(data);
+    })
+    .catch((e) => {
+      res.status(404).send(e);
+    });
 });
